61475f47a9e0fcd74b930c95fde5079d34473759
Tiago Chaves <tiagoloureirochaves@gmail.com>
2020-01-11 13:34:58 -0300

    Outline triangles

--- main.cc
+++ main.cc
@@ -7,32 +7,30 @@ const TGAColor red   = TGAColor(255,   0,   0, 255);
 const TGAColor green = TGAColor(  0, 255,   0, 255);
 const TGAColor blue  = TGAColor(  0,   0, 255, 255);
 
-Model *model = nullptr;
-const int WIDTH  = 800;
-const int HEIGHT = 800;
-const int HALF_WIDTH  = 400;
-const int HALF_HEIGHT = 400;
+const int WIDTH  = 200;
+const int HEIGHT = 200;
 
-void line(int x0, int y0, int x1, int y1, TGAImage &image, const TGAColor &color) {
+void line(Vec2i p0, Vec2i p1, 
+          TGAImage &image, const TGAColor &color) {
     bool transposed = false;
-    if (std::abs(x0 - x1) < std::abs(y0 - y1)) {
-        std::swap(x0, y0); // if the line is steep,
-        std::swap(x1, y1); // we transpose the image
+    if (std::abs(p0.x - p1.x) < std::abs(p0.y - p1.y)) {
+        std::swap(p0.x, p0.y); // if the line is steep,
+        std::swap(p1.x, p1.y); // we transpose the image
         transposed = true;
     }
 
-    if (x0 > x1) {
-        std::swap(x0, x1);
-        std::swap(y0, y1);
+    if (p0.x > p1.x) {
+        std::swap(p0.x, p1.x);
+        std::swap(p0.y, p1.y);
     }
 
-    int dx = x1 - x0;
-    int dy = y1 - y0;
+    int dx = p1.x - p0.x;
+    int dy = p1.y - p0.y;
     int derror2 = std::abs(dy) * 2;
     int error2 = 0;
     
-    int y = y0;
-    for (int x = x0; x <= x1; ++x) {
+    int y = p0.y;
+    for (int x = p0.x; x <= p1.x; ++x) {
         if (transposed)
             image.set(y, x, color); // de−transpose
         else
@@ -40,38 +38,32 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, const TGAColor &color
 
         error2 += derror2;
         if (error2 > dx) {
-            y += (y1 > y0 ? 1 : -1);
+            y += (p1.y > p0.y ? 1 : -1);
             error2 -= dx * 2;
         }
     }
 }
 
-int main(int argc, char **argv) {
-    if (argc >= 2) {
-        model = new Model(argv[1]);
-    } else {
-        model = new Model("obj/head.obj");
-    }
+void triangle(Vec2i t0, Vec2i t1, Vec2i t2, 
+              TGAImage &image, const TGAColor &color) {
+    line(t0, t1, image, color);
+    line(t1, t2, image, color);
+    line(t2, t0, image, color);
+}
 
+int main(int argc, char **argv) {
     TGAImage image(WIDTH, HEIGHT, TGAImage::RGB);
-    for (int i = 0; i < model->nfaces(); ++i) {
-        std::vector<int> face = model->face(i);
-        for (int j = 0; j < 3; ++j) {
-            Vec3f v0 = model->vert(face[j]);
-            Vec3f v1 = model->vert(face[(j + 1) % 3]);
-            
-            int x0 = (v0.x + 1) * HALF_WIDTH;
-            int y0 = (v0.y + 1) * HALF_HEIGHT;
-            int x1 = (v1.x + 1) * HALF_WIDTH;
-            int y1 = (v1.y + 1) * HALF_HEIGHT;
-            
-            line(x0, y0, x1, y1, image, white);
-        }
-    }
+    
+    Vec2i t0[3] = { Vec2i( 10,  70), Vec2i( 50, 160), Vec2i( 70,  80) };
+    Vec2i t1[3] = { Vec2i(180,  50), Vec2i(150,   1), Vec2i( 70, 180) };
+    Vec2i t2[3] = { Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180) };
+
+    triangle(t0[0], t0[1], t0[2], image, red);
+    triangle(t1[0], t1[1], t1[2], image, white);
+    triangle(t2[0], t2[1], t2[2], image, green);
 
     image.flip_vertically(); // have the origin at the bottom-left corner
     image.write_tga_file("output.tga");
 
-    delete model;
     return 0;
 }
